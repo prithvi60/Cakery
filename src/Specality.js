bd06da54201ddf9f4f5e3836e7ac602714da3116
Prithvi-N <prithvin19@gmail.com>
2021-06-02 00:14:15 +0530

    Nav bar Changes

--- src/Specality.js
+++ src/Specality.js
@@ -4,7 +4,7 @@ import './App.css';
 export default function Specality() {
     return (
         <div className="spl-container">  
-        {/* <p className="header">Our Speciality</p>    */}
+        <p className="header">Our Speciality</p>   
         </div>
     )
 }
