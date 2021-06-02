8ede50d9b160ea59516720fd1ec2110e87d0ffc9
Prithvi-N <prithvin19@gmail.com>
2021-06-02 13:27:06 +0530

    Specalities

--- src/Specality.js
+++ src/Specality.js
@@ -3,6 +3,13 @@ import './App.css';
 import Carousel from "react-elastic-carousel";
 import styled from "styled-components";
 
+import {ReactComponent as CurveImg} from "./icons/curve.svg"
+import {ReactComponent as Carousa} from "./icons/carousel1.svg"
+import {ReactComponent as Carousb} from "./icons/carousel2.svg"
+import {ReactComponent as Carousc} from "./icons/carousel3.svg"
+
+
+
 export default function Specality() {
     return (
         <div className="spl-container">  
@@ -11,16 +18,29 @@ export default function Specality() {
         </div>
     )
 }
-
+// Carousel
 const breakPoints = [
     { width: 1, itemsToShow: 1 },
     { width: 550, itemsToShow: 2, itemsToScroll: 2 },
     { width: 768, itemsToShow: 3 },
     { width: 1200, itemsToShow: 4 }
   ];
-  
+  const Item= styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 250px;
+  width: 100%;
+  background-color: transparent;
+  color: #683bb7;
+  margin: 15px;
+  font-size: 4em;
+`;
+
 function ImageCarousel(){
-    const [items, setItems] = useState([1, 2, 3, 4, 5, 6, 7, 8,9]);
+    const [items, setItems] = useState(
+        [<CurveImg/>, <Carousa/>, <Carousb/>, <Carousc/>, 5, 6, 7, 8,9]
+        );
     return(
 
         <div className="carousel-wrapper">
@@ -34,14 +54,3 @@ function ImageCarousel(){
 
 }
 
- const Item= styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  height: 250px;
-  width: 100%;
-  background-color: #683bb7;
-  color: #fff;
-  margin: 15px;
-  font-size: 4em;
-`;
