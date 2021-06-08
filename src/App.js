5f26db91e375845431f490be7a65022d9363379e
Prithvi-N <prithvin19@gmail.com>
2021-06-08 12:50:39 +0530

    Formating

--- src/App.js
+++ src/App.js
@@ -1,33 +1,24 @@
-import React from 'react';
+import React from 'react'
+
 import NavBar from './Components/NavBar';
-import PageUp from './Components/PageUp.js';
 import EmailField from './Components/EmailField';
-import Specality from './Components/Specality';
-import Content from './Components/Content';
-import Creation from './Components/Creation';
-import './App.css';
+import PageUp from './Components/PageUp';
+
+import HomePage from './Views/HomePage';
 
 function App() {
 return(
 <> 
-{/* components */}
-         <PageUp/>
-        <NavBar/>
-        <div className="pic-container">
-        <div className="image-card"> 
-        <div className="icon-logo">
-        <h1 className="icon-text">Welcome to Bellaria</h1>
-        </div> 
-        </div>
-         </div>
-        <Specality/>
-        <Content/>
-        <Creation/>
-          <EmailField/>
+<PageUp/>
+<NavBar/>
+
+{/* router */}
+<HomePage/>
+
+<EmailField/>
         <div className="footer">
           <h1 className="footer-text">Bellaria- A Delicious Cakes and Bakery </h1>
         </div>
-  
 </>
 );
 }
---
+++ src/Views/HomePage.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import Specality from '../Components/Specality';
+import Content from '../Components/Content';
+import Creation from '../Components/Creation';
+import '../App.css';
+
+export default function HomePage() {
+    return (
+        <> 
+{/* components */}
+        <div className="pic-container">
+        <div className="image-card"> 
+        <div className="icon-logo">
+        <h1 className="icon-text">Welcome to Bellaria</h1>
+        </div> 
+        </div>
+         </div>
+        <Specality/>
+        <Content/>
+        <Creation/>
+</>
+    );
+}
