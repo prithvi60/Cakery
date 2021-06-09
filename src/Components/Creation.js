2b66fe998a1f2a6ab1e23ec3e812e6e3f327ed5e
Prithvi-N <prithvin19@gmail.com>
2021-06-09 13:58:31 +0530

    Creations

--- src/Components/Creation.js
+++ src/Components/Creation.js
@@ -1,12 +1,40 @@
-import React from 'react'
+import React,{useState} from 'react'
 import  SplImg from "../images/divider_4.png"
+
+import  Pic1 from "../images/bg1.jpg"
+import  Pic2 from "../images/bg2.jpg"
+import  Pic3 from "../images/bg3.jpg"
+import  Pic4  from "../images/bg4.jpg"
 
 export default function Creation() {
     return (
-        <div className="spl-container">
+        <div className="show-container">
         <img className="header-img"src={SplImg} alt="headimg"/>
         <p className="header">Our Creations</p>  
-            
+        <ImageShow/>
         </div>
     )
+}
+
+function ImageShow(){
+    const [open, setOpen] = useState(false)
+    return(
+        <div className="show-grid">
+        <img className="show-img"src={Pic1} alt="headimg" onClick={()=>setOpen(!open)}/>
+        <ImageDetails/>
+        <img className="show-img"src={Pic2} alt="headimg"/>
+        <img className="show-img"src={Pic3} alt="headimg"/>
+        <img className="show-img"src={Pic4} alt="headimg"/>
+        </div>   
+    )
+}
+
+function ImageDetails({open}){
+if(open) return(
+<div>
+    <p>Loren ipsum solt</p>
+</div>
+    );
+else return null;
+
 }
--- src/Components/PageUp.js
+++ src/Components/PageUp.js
@@ -11,9 +11,7 @@ useEffect(() => {
         <div className="page-up">
             <button id="myBtn" onClick={Up} className="nav-btn">
                 <SrollIcon/>
-            {/* <svg xmlns="http://www.w3.org/2000/svg"  version="1.1"  width="28" height="28" viewBox="0 0 24 24">
-             <path fill="#000000" d="M7.41,15.41L12,10.83L16.59,15.41L18,14L12,8L6,14L7.41,15.41Z" />
-            </svg>  */}
+        
             </button>
         </div>
    
@@ -21,7 +19,7 @@ useEffect(() => {
 }
 
 function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+  if (document.body.scrollTop > 30 || document.documentElement.scrollTop > 30) {
     document.getElementById('myBtn').style.visibility='visible';
   } else {
     document.getElementById('myBtn').style.visibility='hidden';
