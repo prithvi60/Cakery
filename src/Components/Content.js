2d8cf5806d09074e02cff8176fd589e9059736c6
Prithvi-N <prithvin19@gmail.com>
2021-06-13 00:42:07 +0530

    Svg background

--- src/Components/Content.js
+++ src/Components/Content.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import heart from '../images/heart2.png'
 import {ReactComponent as BorderBtn} from '../icons/splbtn.svg'
+// import {ReactComponent as Curvy} from '../images/style_1_1.svg'
 import { Up } from './PageUp'
 export default function Content() {
-    return (
-        <div className="cont-container">
-          
+    return (<>
+        {/* <div className="bg-overlay"></div> */}
+          <div className="cont-container"> 
+         
           <img className="heart" src={heart} alt="logo"/>
           
             <h1>Magic Processing</h1>
@@ -13,6 +15,7 @@ export default function Content() {
             Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Praesent molestie eu turpis nec molestie. Nam auctor magna mauris, non lacinia felis mattis nec.
             </p>
             <button onClick={Up}><BorderBtn fill="#ffffff"/>Discover More</button>
-        </div>
+         </div>
+         </>
     )
 }
