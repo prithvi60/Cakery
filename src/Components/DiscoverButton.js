0fff5f1a3b134322334a60559e901402578600c0
Shahbaz Shaikh <shahbaz.shaikh@devomark.com>
2021-06-16 23:47:15 +0530

    mask image added

---
+++ src/Components/DiscoverButton.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function DiscoverButton() {
+  return (
+    <div className="discover_button_wrapper">
+      <a
+        href="#"
+        id="bellaria_button_5c31fc1a4fb75"
+        className="bellaria_button large figure"
+      >
+        <span></span>Discover More<span></span>
+      </a>
+    </div>
+  );
+}
