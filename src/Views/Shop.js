056cb5a157d0c464a10bc8b9196b2b6751c34dd4
Prithvi-N <prithvin19@gmail.com>
2021-06-23 14:19:27 +0530

    Changes

--- src/Views/Shop.js
+++ src/Views/Shop.js
@@ -1,10 +1,22 @@
 import React from 'react'
 import Slab from '../Components/Slab'
+import  Pic1 from "../images/bg1.jpg"
+import {ReactComponent as CurvyTop} from '../images/shape_copyright_1.svg'
+
+
 
 export default function Shop() {
     return (
         <div>
             <Slab header="Shop" para="Home"/>
+       <div className="shop-container">  
+            <div>
+            <CurvyTop/>
+                </div>
+            <span className="shop-drop">Sort by popularity</span>
+        <img className="show-img"src={Pic1} alt="heading" />
+
+        </div>
         </div>
     )
 }
