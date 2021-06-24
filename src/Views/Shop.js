06d9a65b11b6260b9021fa144cbaffce8c8f0103
Prithvi-N <prithvin19@gmail.com>
2021-06-24 14:19:12 +0530

    Changes

--- src/Views/Shop.js
+++ src/Views/Shop.js
@@ -1,22 +1,71 @@
-import React from 'react'
+import React,{ useState } from 'react'
 import Slab from '../Components/Slab'
+import Creation from '../Components/Creation';
+
+
 import  Pic1 from "../images/bg1.jpg"
+import  Pic2 from "../images/bg2.jpg"
+import  Pic3 from "../images/bg3.jpg"
+import  Pic4  from "../images/bg4.jpg"
+
+
 import {ReactComponent as CurvyTop} from '../images/shape_copyright_1.svg'
 
 
 
 export default function Shop() {
+    const [crationArr] = useState([
+        {
+          img:Pic1,
+          text:"ABC"
+        },
+        {
+          img:Pic2,
+          text:"PQR"
+        },
+        {
+          img:Pic3,
+          text:"PQR"
+        },
+        {
+          img:Pic4,
+          text:"PQR"
+        },
+        {
+          img:Pic1,
+          text:"ABC"
+        },
+        {
+          img:Pic2,
+          text:"PQR"
+        },
+        {
+          img:Pic3,
+          text:"PQR"
+        },
+        {
+          img:Pic4,
+          text:"PQR"
+        },
+        {
+            img:Pic4,
+            text:"PQR"
+         }
+      ])
     return (
-        <div>
+        <>
             <Slab header="Shop" para="Home"/>
-       <div className="shop-container">  
-            <div>
-            <CurvyTop/>
-                </div>
-            <span className="shop-drop">Sort by popularity</span>
-        <img className="show-img"src={Pic1} alt="heading" />
 
+            <div className="show-container">
+            <div>
+                 <CurvyTop/>
+            </div>
+        <div className='creation_wrapper'>
+              {crationArr.map((item,index)=><Creation key={index}
+              text="Add to Cart"
+              img={item.img}/>)}
+            </div>
         </div>
-        </div>
+        </>
     )
 }
